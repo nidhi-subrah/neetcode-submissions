59013d3ca76096bcd9e0f7d01933a78f66b6400c
Nidhi Subrahmanya <nidhisubrahmanya06@gmail.com>
2026-09-10 09:01:59 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s)==Counter(t)
+        
+        
+        #return sorted(s) == sorted(t) #O(nlogn)
+        
