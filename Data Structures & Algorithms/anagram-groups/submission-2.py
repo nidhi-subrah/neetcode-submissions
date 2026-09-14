364f65aed55d511ae43c37cbe0823210e4b8e94a
Nidhi Subrahmanya <nidhisubrahmanya06@gmail.com>
2026-09-13 23:35:53 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map=defaultdict(list)
+        for s in strs:
+            sorted_str="".join(sorted(s))
+            anagram_map[sorted_str].append(s)
+        return list(anagram_map.values())
+            
+        #Time Complexity: O (m nlogn), where m is the number  of strings and n is the maximum length of a string (due to sorting each string).
+
+        #Space Complexity: $O(m \cdot n)$ to store the strings in the hash map.
